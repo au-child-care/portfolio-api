2d6efb2058780c441627b3c5c4dab64dc70d2035
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-03-29 02:46:58 +1100

    Educator statistics integration with milestones

--- app/Http/Controllers/MilestoneController.php
+++ app/Http/Controllers/MilestoneController.php
@@ -29,47 +29,88 @@ class MilestoneController extends Controller
 
     function updateStats($original, $updated) {
         if ($original) {
-            StatisticsAllController::updateStats(array(
-                'total_milestones' => sizeof($original) * -2,
+            $updatePayload = array(
+                'total_milestones' => sizeof($original) * -1,
                 'last_update_mode' => 'Event',
                 'date_modified' => date("Y-m-d H:i:s")
-            ));
+            );
+
+            StatisticsAllController::updateStats($updatePayload);
+
             if (sizeof($original) > 0) {
                 $milestonesCount = array_count_values(array_column($original, 'developmental_area'));
-                StatisticsChildController::updateStats($original[0]['child_id'], array(
-                    'total_milestones' => sizeof($original) * -2,
-                    'total_milestones_physical' => array_key_exists("Physical", $milestonesCount) ?  $milestonesCount['Physical'] * -2 : 0,
-                    'total_milestones_social' => array_key_exists("Social", $milestonesCount) ?  $milestonesCount['Social'] * -2 : 0,
-                    'total_milestones_emotional' => array_key_exists("Emotional", $milestonesCount) ?  $milestonesCount['Emotional'] * -2 : 0,
-                    'total_milestones_cognitive' => array_key_exists("Cognitive", $milestonesCount) ?  $milestonesCount['Cognitive'] * -2 : 0,
-                    'total_milestones_Language' => array_key_exists("Language", $milestonesCount) ?  $milestonesCount['Language'] * -2 : 0,
-                    'total_milestones_seek_advice' => array_key_exists("Seek advice", $milestonesCount) ?  $milestonesCount['Seek advice'] * -2 : 0,
-                    'last_update_mode' => 'Event',
-                    'date_modified' => date("Y-m-d H:i:s")
-                ));
+                $updatePayload['total_milestones_physical'] = array_key_exists("Physical", $milestonesCount) ?  $milestonesCount['Physical'] * -1 : 0;
+                $updatePayload['total_milestones_social'] = array_key_exists("Social", $milestonesCount) ?  $milestonesCount['Social'] * -1 : 0;
+                $updatePayload['total_milestones_emotional'] = array_key_exists("Emotional", $milestonesCount) ?  $milestonesCount['Emotional'] * -1 : 0;
+                $updatePayload['total_milestones_cognitive'] = array_key_exists("Cognitive", $milestonesCount) ?  $milestonesCount['Cognitive'] * -1 : 0;
+                $updatePayload['total_milestones_Language'] = array_key_exists("Language", $milestonesCount) ?  $milestonesCount['Language'] * -1 : 0;
+                $updatePayload['total_milestones_seek_advice'] = array_key_exists("Seek advice", $milestonesCount) ?  $milestonesCount['Seek advice'] * -1 : 0;
+
+                StatisticsChildController::updateStats($original[0]['child_id'], $updatePayload);
+                
+                $educator_ids = array_unique(array_column($original, 'educator_id'));
+                foreach($educator_ids as $educator_id) {
+                    $educatorMilestones = array_filter($original, function($element) use ($educator_id) {
+                        return $element['educator_id'] == $educator_id;
+                    });
+                    $milestonesCount = array_count_values(array_column($educatorMilestones, 'developmental_area'));
+                    $updatePayload = array(
+                        'total_milestones' => sizeof($educatorMilestones) * -1,
+                        'last_update_mode' => 'Event',
+                        'date_modified' => date("Y-m-d H:i:s"),
+                        'total_milestones_physical' => array_key_exists("Physical", $milestonesCount) ?  $milestonesCount['Physical'] * -1 : 0,
+                        'total_milestones_social' => array_key_exists("Social", $milestonesCount) ?  $milestonesCount['Social'] * -1 : 0,
+                        'total_milestones_emotional' => array_key_exists("Emotional", $milestonesCount) ?  $milestonesCount['Emotional'] * -1 : 0,
+                        'total_milestones_cognitive' => array_key_exists("Cognitive", $milestonesCount) ?  $milestonesCount['Cognitive'] * -1 : 0,
+                        'total_milestones_Language' => array_key_exists("Language", $milestonesCount) ?  $milestonesCount['Language'] * -1 : 0,
+                        'total_milestones_seek_advice' => array_key_exists("Seek advice", $milestonesCount) ?  $milestonesCount['Seek advice'] * -1 : 0,
+                    );
+                
+                    StatisticsEducatorController::updateStats($educator_id, $updatePayload); 
+                }
             }
         }
 
-        if ($updated)
-        {
-            StatisticsAllController::updateStats(array(
+        if ($updated) {
+            $updatePayload = array(
                 'total_milestones' => sizeof($updated),
                 'last_update_mode' => 'Event',
                 'date_modified' => date("Y-m-d H:i:s")
-            ));
+            );
+
+            StatisticsAllController::updateStats($updatePayload);
+
             if (sizeof($updated) > 0) {
                 $milestonesCount = array_count_values(array_column($updated, 'developmental_area'));
-                StatisticsChildController::updateStats($updated[0]['child_id'], array(
-                    'total_milestones' => sizeof($updated),
-                    'total_milestones_physical' => array_key_exists("Physical", $milestonesCount) ?  $milestonesCount['Physical'] : 0,
-                    'total_milestones_social' => array_key_exists("Social", $milestonesCount) ?  $milestonesCount['Social'] : 0,
-                    'total_milestones_emotional' => array_key_exists("Emotional", $milestonesCount) ?  $milestonesCount['Emotional'] : 0,
-                    'total_milestones_cognitive' => array_key_exists("Cognitive", $milestonesCount) ?  $milestonesCount['Cognitive'] : 0,
-                    'total_milestones_Language' => array_key_exists("Language", $milestonesCount) ?  $milestonesCount['Language'] : 0,
-                    'total_milestones_seek_advice' => array_key_exists("Seek advice", $milestonesCount) ?  $milestonesCount['Seek advice']  : 0,
-                    'last_update_mode' => 'Event',
-                    'date_modified' => date("Y-m-d H:i:s")
-                ));
+                $updatePayload['total_milestones_physical'] = array_key_exists("Physical", $milestonesCount) ?  $milestonesCount['Physical'] : 0;
+                $updatePayload['total_milestones_social'] = array_key_exists("Social", $milestonesCount) ?  $milestonesCount['Social'] : 0;
+                $updatePayload['total_milestones_emotional'] = array_key_exists("Emotional", $milestonesCount) ?  $milestonesCount['Emotional'] : 0;
+                $updatePayload['total_milestones_cognitive'] = array_key_exists("Cognitive", $milestonesCount) ?  $milestonesCount['Cognitive'] : 0;
+                $updatePayload['total_milestones_Language'] = array_key_exists("Language", $milestonesCount) ?  $milestonesCount['Language'] : 0;
+                $updatePayload['total_milestones_seek_advice'] = array_key_exists("Seek advice", $milestonesCount) ?  $milestonesCount['Seek advice'] : 0;
+
+                StatisticsChildController::updateStats($updated[0]['child_id'], $updatePayload);
+                
+                $educator_ids = array_unique(array_column($updated, 'educator_id'));
+                foreach($educator_ids as $educator_id) {
+                    $educatorMilestones = array_filter($updated, function($element) use ($educator_id) {
+                        return $element['educator_id'] == $educator_id;
+                    });
+                    $milestonesCount = array_count_values(array_column($educatorMilestones, 'developmental_area'));
+                    $updatePayload = array(
+                        'total_milestones' => sizeof($educatorMilestones),
+                        'last_update_mode' => 'Event',
+                        'date_modified' => date("Y-m-d H:i:s"),
+                        'total_milestones_physical' => array_key_exists("Physical", $milestonesCount) ?  $milestonesCount['Physical'] : 0,
+                        'total_milestones_social' => array_key_exists("Social", $milestonesCount) ?  $milestonesCount['Social'] : 0,
+                        'total_milestones_emotional' => array_key_exists("Emotional", $milestonesCount) ?  $milestonesCount['Emotional'] : 0,
+                        'total_milestones_cognitive' => array_key_exists("Cognitive", $milestonesCount) ?  $milestonesCount['Cognitive'] : 0,
+                        'total_milestones_Language' => array_key_exists("Language", $milestonesCount) ?  $milestonesCount['Language'] : 0,
+                        'total_milestones_seek_advice' => array_key_exists("Seek advice", $milestonesCount) ?  $milestonesCount['Seek advice'] : 0,
+                    );
+                
+                    StatisticsEducatorController::updateStats($educator_id, $updatePayload); 
+                }
             }
         }
     }
