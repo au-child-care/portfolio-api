99654f0d4f52d78f3ed957a998ee39870b9df5bd
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-03-28 23:41:39 +1100

    Child statistics integration with observations

--- app/Http/Controllers/ObservationController.php
+++ app/Http/Controllers/ObservationController.php
@@ -66,15 +66,38 @@ class ObservationController extends Controller
                 'last_update_mode' => 'Event',
                 'date_modified' => $updated['date_modified']
             ));
+            if ($original['published'] == 1) {
+                StatisticsChildController::updateStats($original['child_id'], array(
+                    'total_observations' => -1,
+                    'total_observations_outcome1' => $original['outcome_id'] == '1' ? -1 : 0,
+                    'total_observations_outcome2' => $original['outcome_id'] == '2' ? -1 : 0,
+                    'total_observations_outcome3' => $original['outcome_id'] == '3' ? -1 : 0,
+                    'total_observations_outcome4' => $original['outcome_id'] == '4' ? -1 : 0,
+                    'total_observations_outcome5' => $original['outcome_id'] == '5' ? -1 : 0,
+                    'last_update_mode' => 'Event',
+                    'date_modified' => $updated['date_modified']
+                )); 
+            }
         }
 
-        if ($updated['deleted'] === 0)
-        {
+        if ($updated['deleted'] == 0) {
             StatisticsAllController::updateStats(array(
                 'total_observations' => 1,
                 'last_update_mode' => 'Event',
                 'date_modified' => $updated['date_modified']
             ));
+            if ($updated['published'] == 1) {
+                StatisticsChildController::updateStats($updated['child_id'], array(
+                    'total_observations' => 1,
+                    'total_observations_outcome1' => $updated['outcome_id'] == '1' ? 1 : 0,
+                    'total_observations_outcome2' => $updated['outcome_id'] == '2' ? 1 : 0,
+                    'total_observations_outcome3' => $updated['outcome_id'] == '3' ? 1 : 0,
+                    'total_observations_outcome4' => $updated['outcome_id'] == '4' ? 1 : 0,
+                    'total_observations_outcome5' => $updated['outcome_id'] == '5' ? 1 : 0,
+                    'last_update_mode' => 'Event',
+                    'date_modified' => $updated['date_modified']
+                ));
+            }
         }
     }
 }
