2b7d0d2bce5b5a8f8fef27ae8505f79a27c1d865
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-04-02 02:50:35 +1100

    Milestones utilities

---
+++ app/Utilities/MilestonesUtilities.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Utilities;
+
+class MilestonesUtilities {
+    public static function getMilestonesQuota() {
+        return array(
+            array(
+                'min_month' => 0,
+                'max_month' => 3,
+                'age_group' => 'Birth to 4 months',
+                'developmental_area' => 'Physical',
+                'total' => 12
+            ),
+            array(
+                'min_month' => 0,
+                'max_month' => 3,
+                'age_group' => 'Birth to 4 months',
+                'developmental_area' => 'Social',
+                'total' => 5
+            ),
+            array(
+                'min_month' => 0,
+                'max_month' => 3,
+                'age_group' => 'Birth to 4 months',
+                'developmental_area' => 'Emotional',
+                'total' => 4
+            ),
+            array(
+                'min_month' => 0,
+                'max_month' => 3,
+                'age_group' => 'Birth to 4 months',
+                'developmental_area' => 'Cognitive',
+                'total' => 7
+            ),
+            array(
+                'min_month' => 0,
+                'max_month' => 3,
+                'age_group' => 'Birth to 4 months',
+                'developmental_area' => 'Language',
+                'total' => 7
+            ),
+
+            array(
+                'min_month' => 4,
+                'max_month' => 7,
+                'age_group' => '4 to 8 months',
+                'developmental_area' => 'Physical',
+                'total' => 11
+            ),
+            array(
+                'min_month' => 4,
+                'max_month' => 7,
+                'age_group' => '4 to 8 months',
+                'developmental_area' => 'Social',
+                'total' => 4
+            ),
+            array(
+                'min_month' => 4,
+                'max_month' => 7,
+                'age_group' => '4 to 8 months',
+                'developmental_area' => 'Emotional',
+                'total' => 6
+            ),
+            array(
+                'min_month' => 4,
+                'max_month' => 7,
+                'age_group' => '4 to 8 months',
+                'developmental_area' => 'Cognitive',
+                'total' => 11
+            ),
+            array(
+                'min_month' => 4,
+                'max_month' => 7,
+                'age_group' => '4 to 8 months',
+                'developmental_area' => 'Language',
+                'total' => 6
+            ),
+
+            array(
+                'min_month' => 8,
+                'max_month' => 11,
+                'age_group' => '8 to 12 months',
+                'developmental_area' => 'Physical',
+                'total' => 18
+            ),
+            array(
+                'min_month' => 8,
+                'max_month' => 11,
+                'age_group' => '8 to 12 months',
+                'developmental_area' => 'Social',
+                'total' => 1
+            ),
+            array(
+                'min_month' => 8,
+                'max_month' => 11,
+                'age_group' => '8 to 12 months',
+                'developmental_area' => 'Emotional',
+                'total' => 5
+            ),
+            array(
+                'min_month' => 8,
+                'max_month' => 11,
+                'age_group' => '8 to 12 months',
+                'developmental_area' => 'Cognitive',
+                'total' => 12
+            ),
+            array(
+                'min_month' => 8,
+                'max_month' => 11,
+                'age_group' => '8 to 12 months',
+                'developmental_area' => 'Language',
+                'total' => 9
+            ),
+
+            array(
+                'min_month' => 12,
+                'max_month' => 23,
+                'age_group' => '1 to 2 years',
+                'developmental_area' => 'Physical',
+                'total' => 17
+            ),
+            array(
+                'min_month' => 12,
+                'max_month' => 23,
+                'age_group' => '1 to 2 years',
+                'developmental_area' => 'Social',
+                'total' => 3
+            ),
+            array(
+                'min_month' => 12,
+                'max_month' => 23,
+                'age_group' => '1 to 2 years',
+                'developmental_area' => 'Emotional',
+                'total' => 5
+            ),
+            array(
+                'min_month' => 12,
+                'max_month' => 23,
+                'age_group' => '1 to 2 years',
+                'developmental_area' => 'Cognitive',
+                'total' => 12
+            ),
+            array(
+                'min_month' => 12,
+                'max_month' => 23,
+                'age_group' => '1 to 2 years',
+                'developmental_area' => 'Language',
+                'total' => 6
+            ),
+
+            array(
+                'min_month' => 24,
+                'max_month' => 35,
+                'age_group' => '2 to 3 years',
+                'developmental_area' => 'Physical',
+                'total' => 16
+            ),
+            array(
+                'min_month' => 24,
+                'max_month' => 35,
+                'age_group' => '2 to 3 years',
+                'developmental_area' => 'Social',
+                'total' => 4
+            ),
+            array(
+                'min_month' => 24,
+                'max_month' => 35,
+                'age_group' => '2 to 3 years',
+                'developmental_area' => 'Emotional',
+                'total' => 5
+            ),
+            array(
+                'min_month' => 24,
+                'max_month' => 35,
+                'age_group' => '2 to 3 years',
+                'developmental_area' => 'Cognitive',
+                'total' => 13
+            ),
+            array(
+                'min_month' => 24,
+                'max_month' => 35,
+                'age_group' => '2 to 3 years',
+                'developmental_area' => 'Language',
+                'total' => 9
+            ),
+
+            array(
+                'min_month' => 36,
+                'max_month' => 60,
+                'age_group' => '3 to 5 years',
+                'developmental_area' => 'Physical',
+                'total' => 17
+            ),
+            array(
+                'min_month' => 36,
+                'max_month' => 60,
+                'age_group' => '3 to 5 years',
+                'developmental_area' => 'Social',
+                'total' => 5
+            ),
+            array(
+                'min_month' => 36,
+                'max_month' => 60,
+                'age_group' => '3 to 5 years',
+                'developmental_area' => 'Emotional',
+                'total' => 7
+            ),
+            array(
+                'min_month' => 36,
+                'max_month' => 60,
+                'age_group' => '3 to 5 years',
+                'developmental_area' => 'Cognitive',
+                'total' => 17
+            ),
+            array(
+                'min_month' => 36,
+                'max_month' => 60,
+                'age_group' => '3 to 5 years',
+                'developmental_area' => 'Language',
+                'total' => 10
+            )
+        );
+    }
+}
