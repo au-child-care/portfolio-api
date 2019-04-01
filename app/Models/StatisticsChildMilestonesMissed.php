c65259109c009b79c17e0d2646488ae0c41fc8f5
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-04-01 23:14:52 +1100

    Statistics child - milestones missed model

---
+++ app/Models/StatisticsChildMilestonesMissed.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StatisticsChildMilestonesMissed extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'child_id',
+        'current_group',
+        'milestones_missed',
+        'total',
+        'last_update_mode',
+        'date_modified'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [ ];
+    
+    protected $table = 'stats_children_milestones_missed';
+
+    protected $primaryKey = 'child_id';
+
+    public $timestamps = false;
+}
