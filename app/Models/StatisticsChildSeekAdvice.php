37a0ef38bef0d1bc4c42cadc6d55f905327ba7f6
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-04-01 23:00:28 +1100

    Child stats - seek advice model

---
+++ app/Models/StatisticsChildSeekAdvice.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StatisticsChildSeekAdvice extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'child_id',
+        'current_group',
+        'milestone_age_group',
+        'count',
+        'last_update_mode',
+        'date_modified'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [ ];
+    
+    protected $table = 'stats_children_seek_advice';
+
+    protected $primaryKey = 'child_id';
+
+    public $timestamps = false;
+}
